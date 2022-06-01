e49a5b98023561b883b10719668f269a29630564
Giuseppe-lore <loregiuseppe0@gmail.com>
2022-06-01 15:21:59 +0200

    done

--- js/script.js
+++ js/script.js
@@ -19,11 +19,11 @@ discountOver65 = userSubtotal * 40 / 100;
 
 // The output of the final price must be put out with a maximum of two decimals:
 if(userAge < '18') {
-    userTicketPrice = 'Hai meno di 18 anni, quindi puoi usufruire del 20% di sconto! Il prezzo del tuo viaggio è pari ad euro  ' + (userSubtotal - discountOver18).toFixed(2) + ' .Grazie per aver scelto JSItalia!'
+    userTicketPrice = 'Hai meno di 18 anni, quindi puoi usufruire del 20% di sconto! Il prezzo del tuo viaggio è pari ad euro  ' + (userSubtotal - discountOver18).toFixed(2) + '.   Arrivederci e grazie per aver scelto JSItalia!'
 } else if(userAge > '65') {
-    userTicketPrice = 'Ha più di 65 anni, quindi può usufruire del 40% di sconto! Il prezzo del suo viaggio è pari ad euro  ' + (userSubtotal - discountOver65).toFixed(2) + ' .Grazie per aver scelto JSItalia!'
+    userTicketPrice = 'Ha più di 65 anni, quindi può usufruire del 40% di sconto! Il prezzo del suo viaggio è pari ad euro  ' + (userSubtotal - discountOver65).toFixed(2) + '.   Arrivederci e grazie per aver scelto JSItalia!'
 } else {
-    userTicketPrice = 'Il prezzo del suo viaggio è pari ad euro  ' + (userSubtotal.toFixed(2)) + ' .Grazie per aver scelto JSItalia!'
+    userTicketPrice = 'Il prezzo del suo viaggio è pari ad euro  ' + (userSubtotal.toFixed(2)) + '.   Arrivederci e grazie per aver scelto JSItalia!'
 }
 
 
